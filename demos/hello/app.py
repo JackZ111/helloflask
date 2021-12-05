2b5a95e89dba0aa10c8dc563e8451a4a9007c28f
zs <690839422@qq.com>
2021-12-05 14:40:40 +0800

    添加部分备注

--- demos/hello/app.py
+++ demos/hello/app.py
@@ -6,27 +6,27 @@
     :license: MIT, see LICENSE for more details.
 """
 import click
-from flask import Flask
+from flask import Flask  # 导入Flask类
 
-app = Flask(__name__)
+app = Flask(__name__)  # 实例类得到app实例，传入特殊变量__name__（此处＝app）
 
 
 # the minimal Flask application
-@app.route('/')
-def index():
+@app.route('/')  # app.route装饰器/注册路由/路由负责管理URL和函数之间的映射
+def index():  # 视图函数
     return '<h1>Hello, World!</h1>'
 
 
 # bind multiple URL for one view function
 @app.route('/hi')
-@app.route('/hello')
+@app.route('/hello')  # 一个视图函数一次性绑定多个URL
 def say_hello():
     return '<h1>Hello, Flask!</h1>'
 
 
 # dynamic route, URL variable default
 @app.route('/greet', defaults={'name': 'Programmer'})
-@app.route('/greet/<name>')
+@app.route('/greet/<name>') # 视图函数动态绑定URL，两种不同绑定方式，第二种若用户未输入name变量会返回404
 def greet(name):
     return '<h1>Hello, %s!</h1>' % name
 
